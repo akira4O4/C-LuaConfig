9d7b3c7a612e1d8bc7b972458e41a3cc1cd14008
akira.404 <akira.404@foxmail.com>
2025-03-23 01:09:25 +0800

    update

---
+++ main.cc
@@ -0,0 +1,8 @@
+#include "src/luaconfig.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Luaconfig"<<std::endl; 
+    return 0;
+}
---
+++ src/luaconfig.h
@@ -0,0 +1,4 @@
+#ifndef _LUA_CONFIG_H_
+#define _LUA_CONFIG_H_
+
+#endif 
